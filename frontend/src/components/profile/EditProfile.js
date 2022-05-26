90944821b12d4456ae8bcd41ec2ad3221346179a
sampanna-2 <sampanna-2>
2022-05-26 13:43:58 +0545

    Edit profile

---
+++ frontend/src/components/profile/EditProfile.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { checkImage } from '../../utils/imageUpload'
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+import { updateProfileUser } from '../../redux/actions/profileAction'
+
+const EditProfile = ({ setOnEdit }) => {
+    
+}
+
+export default EditProfile
