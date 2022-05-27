d45727f3414a7c0c5afec27843caae7fdb6bda73
sampanna-2 <sampanna-2>
2022-05-27 09:51:18 +0545

    update on edit profile page

--- frontend/src/components/profile/EditProfile.js
+++ frontend/src/components/profile/EditProfile.js
@@ -5,6 +5,41 @@ import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 import { updateProfileUser } from '../../redux/actions/profileAction'
 
 const EditProfile = ({ setOnEdit }) => {
+const initState = {
+        fullname: '', mobile: '', address: '', website: '', story: '', gender: ''
+    }
+    const [userData, setUserData] = useState(initState)
+    const { fullname, mobile, address, website, story, gender } = userData
+
+    const [avatar, setAvatar] = useState('')
+
+    const { auth, theme } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        setUserData(auth.user)
+    }, [auth.user])
+    
+    const changeAvatar = (e) => {
+        const file = e.target.files[0]
+
+        const err = checkImage(file)
+        if (err) return dispatch({
+            type: GLOBALTYPES.ALERT, payload: { error: err }
+        })
+
+        setAvatar(file)
+    }
+
+    const handleInput = e => {
+        const { name, value } = e.target
+        setUserData({ ...userData, [name]: value })
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        dispatch(updateProfileUser({ userData, avatar, auth }))
+    }
+    
     
 }
 
