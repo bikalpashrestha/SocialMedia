41987ff9065a69b2d7b3aeb60de078284d2a75a0
aairothapaliya <aairo2077@gmail.com>
2022-06-01 00:06:19 +0545

    data fetch..

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -7,3 +7,16 @@ export const getDataAPI = async (url, token) => {
     return res;
 }
 
+export const postDataAPI = async (url, post, token) => {
+    const res = await axios.post(`/api/${url}`, post, {
+        headers: { Authorization: token}
+    })
+    return res;
+}
+
+export const putDataAPI = async (url, post, token) => {
+    const res = await axios.put(`/api/${url}`, post, {
+        headers: { Authorization: token}
+    })
+    return res;
+}
