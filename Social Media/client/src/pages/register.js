5dac67ef1a608259035f3fe506506ffb2cdd87e6
bigyan815 <bigyantamang.90@gmail.com>
2022-06-02 20:56:52 +0545

    User Into Profile

--- Social Media/client/src/pages/register.js
+++ Social Media/client/src/pages/register.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {useHistory} from 'react-router-dom'
 
 const Register = () => {
+  const {} = useSelector(state => state)
+  const history = useHistory()
+
+  useEffect(() => {
+    if(auth.token) history.push("/")
+  }, [auth.token, history])
+
+  
   return (
     <div>R</div>
   )
---
+++ Social Media/client/src/redux/reducers/profileReducer.js
@@ -0,0 +1,13 @@
+import { PROFILE_TYPES} from '../actions/profileAction'
+
+const initialState = {
+    loading: false,
+    users: [],
+    posts: []
+}
+
+const profileReducer = (state = initialState, action) => {
+    switch (action.type){
+        case.PROFILE_TYPES.LOADING
+    }
+}
