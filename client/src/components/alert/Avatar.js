e9324a549947237c3eef223710d34b7f98fd047c
sampanna-2 <sampanna-2>
2022-06-02 16:07:03 +0545

    Edit profile

---
+++ client/src/components/alert/Avatar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const Avatar = ({src}) => {
+    const { theme } = useSelector(state => state)
+
+    return (
+            <img src={src} alt="avatar" className="avatar"
+            style={{filter: `${theme ? 'invert(1)' : 'invert(0)'}`}} />
+    )
+}
