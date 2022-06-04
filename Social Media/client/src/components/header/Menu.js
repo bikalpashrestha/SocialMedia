6374c369df70a64719c78510fc1ff92a22e2a7a2
bibek788 <70308531+bibek788@users.noreply.github.com>
2022-06-04 17:19:40 +0545

    menu

--- Social Media/client/src/components/header/Menu.js
+++ Social Media/client/src/components/header/Menu.js
@@ -33,5 +33,34 @@ const Menu = () => {
                     </li>
                 ))
             }
+             
+             <li className="nav-item dropdown">
+                <span className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <Avatar src={auth.user.avatar} size="medium-avatar" />
+                </span>
 
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <Link className="dropdown-item" to={`/profile/${auth.user._id}`}>Profile</Link>
+
+                <label htmlFor="theme" className="dropdown-item"
+                    onClick={() => dispatch({
+                        type: GLOBALTYPES.THEME, payload: !theme
+                    })}>
+
+                    {theme ? 'Light mode' : 'Dark mode'}
+                </label>
+                
+                <div className="dropdown-divider"></div>
+                <Link className="dropdown-item" to="/"
+                    onClick={() => dispatch(logout())}>
+                    Logout
+                </Link>
+            </div>
+        </li>
+    </ul>
+</div>
+  )
+}
+
+export default Menu
          
