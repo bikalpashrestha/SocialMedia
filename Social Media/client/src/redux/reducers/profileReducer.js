f72750a2bb90bb0b6c3b7001c288ae9b843cad22
bigyan815 <bigyantamang.90@gmail.com>
2022-06-07 19:24:45 +0545

    Create Comment started

--- Social Media/client/src/redux/reducers/profileReducer.js
+++ Social Media/client/src/redux/reducers/profileReducer.js
@@ -1,11 +1,11 @@
 import { PROFILE_TYPES } from '../actions/profileAction'
+import { EditData } from '../actions/globalTypes';
 
 const initialState = {
     loading: false,
     users: [],
     posts: []
 }
-
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case PROFILE_TYPES.LOADING:
@@ -18,10 +18,18 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 users: [...state.users, action.payload.user]
             };
+        case PROFILE_TYPES.FOLLOW:
+            return {
+                ...state,
+                users: EditData(state.users, action.payload._id, action.payload)
+            };
+        case PROFILE_TYPES.UNFOLLOW:
+            return {
+                ...state,
+                users: EditData(state.users, action.payload._id, action.payload)
+            };
         default:
             return state;
-
     }
 }
-
 export default profileReducer
