e62c6cc9e93bebad544ccd5221dd1857448d0c66
aairothapaliya <aairo2077@gmail.com>
2022-06-07 14:19:06 +0545

    authctrl

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -68,3 +68,43 @@ login: async (req, res) => {
                 path: '/api/refresh_token',
                 maxAge: 30*24*60*60*1000 // 30days
             })
+res.json({
+                msg: 'Login Success!',
+                access_token,
+                user: {
+                    ...user._doc,
+                    password: ''
+                }
+            })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', {path: '/api/refresh_token'})
+            return res.json({msg: "Logged out!"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    generateAccessToken: async (req, res) => {
+        try {
+            const rf_token = req.cookies.refreshtoken
+            if(!rf_token) return res.status(400).json({msg: "Please login now."})
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
+                if(err) return res.status(400).json({msg: "Please login now."})
+
+                const user = await Users.findById(result.id).select("-password")
+                .populate('followers following', 'avatar username fullname followers following')
+
+                if(!user) return res.status(400).json({msg: "This does not exist."})
+
+                const access_token = createAccessToken({id: result.id})
+
+                res.json({
+                    access_token,
+                    user
+                })
+            })
