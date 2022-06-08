653e6bd85a701230cd86b33ced643f60160aee83
bigyan815 <bigyantamang.90@gmail.com>
2022-06-08 21:28:00 +0545

    Input Comment

--- Social Media/client/src/components/home/InputComment.js
+++ Social Media/client/src/components/home/InputComment.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { createComment } from '../../redux/actions/commentAction'
 
 const InputComment = ({children}) => {
     const [content, setContent] = useState('')
+
+    const { auth } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!content.trim()) return;
+
+        setContent('')
+
+        const newComment = {
+            content,
+            likes: [],
+            user: auth.user,
+            createdAt: new Date().toISOString()
+        }
+        dispatch(createComment(post, newComment, auth))
+    }
     return (
-        <form>
+        <form className='card-footer comment_input' onSubmit={handleSubmit}>
             {children}
             <input type="text" placeholder="Add your comments..."
-            value={content} onChange={e => setContent(e.target.value)} />
+            value={content} onChange={e => setContent(e.target.value)}/>
 
-            <button type="submit" className="postBtn">
-                Postic
+            <button type='submit' className='postBtn'>
+                post
             </button>
         </form>
-    )
+  )
 }
 
-export default InputComment 
+export default InputComment
