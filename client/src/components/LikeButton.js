f9fe68385f621198c925a58e65d5768d59cf7b14
sampanna-2 <sampanna-2>
2022-06-10 17:19:33 +0545

    edit on likebutton

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const LikeButton = ({isLike, handleLike, handleUnLike}) => {
+    const { theme } = useSelector(state => state)
+
+    return (
+        
+    )
+}
+
+export default LikeButton
