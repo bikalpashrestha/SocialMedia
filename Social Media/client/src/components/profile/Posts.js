3a0d99c54ced84a1085f8ab79d8201330c6dabb1
bigyan815 <bigyantamang.90@gmail.com>
2022-06-11 11:46:23 +0545

    Saved Post

--- Social Media/client/src/components/profile/Posts.js
+++ Social Media/client/src/components/profile/Posts.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PostThumb from '../PostThumb'
+import LoadIcon from '../../images/loading.gif'
+import LoadMoreBtn from '../LoadMoreBtn'
+import { getDataAPI } from '../../utils/fetchData'
+import { PROFILE_TYPES } from '../../redux/actions/profileAction'
 
-const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
-}
-
-export default Posts
+const Posts = ({auth, id, dispatch, profile}) => {
+    const [posts, setPosts] = useState([])
+    const [result, setResult] = useState(9)
+    const [page, setPage] = useState(0)
+    const [load, setLoad] = useState(false)
