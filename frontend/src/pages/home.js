02cc85ba5b63039f033f0a97a65d43415dbf5155
aairothapaliya <aairo2077@gmail.com>
2022-06-12 15:37:17 +0545

    home and status

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -20,4 +20,30 @@ const Home = () => {
         }
     })
 
+
+ return (
+        <div className="home row mx-0">
+            <div className="col-md-8">
+                <Status />
+
+                {
+                    homePosts.loading 
+                    ? <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
+                    : (homePosts.result === 0 && homePosts.posts.length === 0)
+                        ? <h2 className="text-center">No Post</h2>
+                        : <Posts />
+                }
+                
+            </div>
+            
+            <div className="col-md-4"> 
+                <RightSideBar />
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
+
     
