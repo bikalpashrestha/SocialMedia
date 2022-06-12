b2f4eb50ec8712cbe3f0c4c4b6650fc85eef09b4
aairothapaliya <aairo2077@gmail.com>
2022-06-12 23:39:40 +0545

    home post statusmodal

--- frontend/src/components/profile/Posts.js
+++ frontend/src/components/profile/Posts.js
@@ -4,7 +4,9 @@ import { PROFILE_TYPES } from '../../redux/actions/profileAction'
 
 const Posts = ({auth, id, dispatch, profile}) => {
     const [posts, setPosts] = useState([])
-   
+    const [result, setResult] = useState(9)
+    const [page, setPage] = useState(0)
+    const [load, setLoad] = useState(false)
 
     useEffect(() => {
         profile.posts.forEach(data => {
@@ -15,6 +17,15 @@ const Posts = ({auth, id, dispatch, profile}) => {
             }
         })
     },[profile.posts, id])
+   
+ const handleLoadMore = async () => {
+        setLoad(true)
+        const res = await getDataAPI(`user_posts/${id}?limit=${page * 9}`, auth.token)
+        const newData = {...res.data, page: page + 1, _id: id}
+        dispatch({type: PROFILE_TYPES.UPDATE_POST, payload: newData})
+        setLoad(false)
+    }
+
 
    
     return (
