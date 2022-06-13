fa2dda7dd5df4b3c4b3da79f1277aa23aa497c26
bibek788 <70308531+bibek788@users.noreply.github.com>
2022-06-13 13:28:36 +0545

    profile suggestion

--- frontend/components/home/Comments.js
+++ frontend/components/home/Comments.js
@@ -19,32 +19,4 @@ const Comments = ({post}) => {
         setReplyComments(newRep)
     }, [post.comments])
 
-    return (
-        <div className="comments">
-            {
-                showComments.map((comment, index) => (
-                    <CommentDisplay key={index} comment={comment} post={post}
-                    replyCm={replyComments.filter(item => item.reply === comment._id)} />
-                ))
-            }
-
-            {
-                comments.length - next > 0
-                ? <div className="p-2 border-top"
-                style={{cursor: 'pointer', color: 'crimson'}}
-                onClick={() => setNext(next + 10)}>
-                    See more comments...
-                </div>
-
-                : comments.length > 2 &&
-                <div className="p-2 border-top"
-                style={{cursor: 'pointer', color: 'crimson'}}
-                onClick={() => setNext(2)}>
-                    Hide comments...
-                </div>
-            }
-        </div>
-    )
-}
-
-export default Comments
+   
