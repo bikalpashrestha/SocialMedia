267874832bed112a7118a5279aaceafae6d0ec35
Rachhya Sherchan <rachhyasherchan1@gmail.com>
2022-06-15 21:29:04 +0545

    like

--- SocialMedia-master/client/public/profile/LikeButton.js
+++ SocialMedia-master/client/public/profile/LikeButton.js
@@ -1,15 +1,19 @@
 import React from 'react'
-const LikeButton=({isLike})=>{
-    return(
+import { useSelector } from 'react-redux'
+
+const LikeButton = ({isLike, handleLike, handleUnLike}) => {
+    const { theme } = useSelector(state => state)
+
+    return (
         <>
-        {
-            isLike
-            ?<i className="far fa=heart"/>
-            :<i className="far fa-heart"/>
-        }
+            {
+                isLike
+                ? <i className="fas fa-heart text-danger" onClick={handleUnLike}
+                style={{filter: theme ? 'invert(1)' : 'invert(0)'}} />
+                : <i className="far fa-heart" onClick={handleLike} />
+            }
         </>
     )
-
-    
 }
+
 export default LikeButton
