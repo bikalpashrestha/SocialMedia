b177dd842ea13cee42de55ad9ed28e6e631c42e2
bigyan815 <bigyantamang.90@gmail.com>
2022-06-19 21:22:59 +0545

    notify Action updated

--- Social Media/client/src/redux/actions/notifyAction.js
+++ Social Media/client/src/redux/actions/notifyAction.js
@@ -25,4 +25,42 @@ export const createNotify = ({msg, auth, socket}) => async (dispatch) => {
         dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
     }
 }
+export const removeNotify = ({msg, auth, socket}) => async (dispatch) => {
+    try {
+        await deleteDataAPI(`notify/${msg.id}?url=${msg.url}`, auth.token)
+        
+        socket.emit('removeNotify', msg)
+    } catch (err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+    }
+}
+
+export const getNotifies = (token) => async (dispatch) => {
+    try {
+        const res = await getDataAPI('notifies', token)
+        
+        dispatch({ type: NOTIFY_TYPES.GET_NOTIFIES, payload: res.data.notifies })
+    } catch (err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+    }
+}
+
+
+export const isReadNotify = ({msg, auth}) => async (dispatch) => {
+    dispatch({type: NOTIFY_TYPES.UPDATE_NOTIFY, payload: {...msg, isRead: true}})
+    try {
+        await patchDataAPI(`/isReadNotify/${msg._id}`, null, auth.token)
+    } catch (err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+    }
+}
+
+export const deleteAllNotifies = (token) => async (dispatch) => {
+    dispatch({type: NOTIFY_TYPES.DELETE_ALL_NOTIFIES, payload: []})
+    try {
+        await deleteDataAPI('deleteAllNotify', token)
+    } catch (err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+    }
+}
 
