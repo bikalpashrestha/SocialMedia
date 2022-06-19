9db393585eee1c7c390e5f6cdc74ee7af5368fdb
bigyan815 <bigyantamang.90@gmail.com>
2022-06-19 10:09:08 +0545

    notify Action

---
+++ Social Media/client/src/redux/actions/notifyAction.js
@@ -0,0 +1,28 @@
+import { GLOBALTYPES } from './globalTypes'
+import { postDataAPI, deleteDataAPI, getDataAPI, patchDataAPI } from '../../utils/fetchData'
+
+export const NOTIFY_TYPES = {
+    GET_NOTIFIES: 'GET_NOTIFIES',
+    CREATE_NOTIFY: 'CREATE_NOTIFY',
+    REMOVE_NOTIFY: 'REMOVE_NOTIFY',
+    UPDATE_NOTIFY: 'UPDATE_NOTIFY',
+    UPDATE_SOUND: 'UPDATE_SOUND',
+    DELETE_ALL_NOTIFIES: 'DELETE_ALL_NOTIFIES'
+}
+
+export const createNotify = ({msg, auth, socket}) => async (dispatch) => {
+    try {
+        const res = await postDataAPI('notify', msg, auth.token)
+
+        socket.emit('createNotify', {
+            ...res.data.notify,
+            user: {
+                username: auth.user.username,
+                avatar: auth.user.avatar
+            }
+        })
+    } catch (err) {
+        dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+    }
+}
+
