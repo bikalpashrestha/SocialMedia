e5ee72c43ef72c3545e2aafa36c2480709b42152
bigyan815 <bigyantamang.90@gmail.com>
2022-06-21 21:47:16 +0545

    notify modal updated

--- Social Media/client/src/components/NotifyModal.js
+++ Social Media/client/src/components/NotifyModal.js
@@ -18,4 +18,29 @@ const NotifyModal = () => {
         dispatch({ type: NOTIFY_TYPES.UPDATE_SOUND, payload: !notify.sound })
     }
 
+    const handleDeleteAll = () => {
+        const newArr = notify.data.filter(item => item.isRead === false)
+        if (newArr.length === 0) return dispatch(deleteAllNotifies(auth.token))
+
+        if (window.confirm(`You have ${newArr.length} unread notices. Are you sure you want to delete all?`)) {
+            return dispatch(deleteAllNotifies(auth.token))
+        }
+    }
+
+    return (
+        <div style={{ minWidth: '300px' }}>
+            <div className="d-flex justify-content-between align-items-center px-3">
+                <h3>Notification</h3>
+                {
+                    notify.sound
+                        ? <i className="fas fa-bell text-danger"
+                            style={{ fontSize: '1.2rem', cursor: 'pointer' }}
+                            onClick={handleSound} />
+
+                        : <i className="fas fa-bell-slash text-danger"
+                            style={{ fontSize: '1.2rem', cursor: 'pointer' }}
+                            onClick={handleSound} />
+                }
+            </div>
+            <hr className="mt-0" />
    
