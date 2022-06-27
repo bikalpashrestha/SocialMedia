5df596692eb277afd8477eeb59d93b6c838e00f0
bibek788 <70308531+bibek788@users.noreply.github.com>
2022-06-27 22:33:26 +0545

    video and icon

--- frontend/components/StatusModal.js
+++ frontend/components/StatusModal.js
@@ -5,3 +5,54 @@ import { createPost, updatePost } from '../redux/actions/postAction'
 import Icons from './Icons'
 import { imageShow, videoShow } from '../utils/mediaShow'
 
+
+const StatusModal = () => {
+    const { auth, theme, status, socket } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const [content, setContent] = useState('')
+    const [images, setImages] = useState([])
+
+    const [stream, setStream] = useState(false)
+    const videoRef = useRef()
+    const refCanvas = useRef()
+    const [tracks, setTracks] = useState('')
+
+    const handleChangeImages = e => {
+        const files = [...e.target.files]
+        let err = ""
+        let newImages = []
+
+        files.forEach(file => {
+            if(!file) return err = "File does not exist."
+
+            if(file.size > 1024 * 1024 * 5){
+                return err = "The image/video largest is 5mb."
+            }
+
+            return newImages.push(file)
+        })
+
+        if(err) dispatch({ type: GLOBALTYPES.ALERT, payload: {error: err} })
+        setImages([...images, ...newImages])
+    }
+
+    const deleteImages = (index) => {
+        const newArr = [...images]
+        newArr.splice(index, 1)
+        setImages(newArr)
+    }
+
+    const handleStream = () => {
+        setStream(true)
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+            navigator.mediaDevices.getUserMedia({video: true})
+            .then(mediaStream => {
+                videoRef.current.srcObject = mediaStream
+                videoRef.current.play()
+
+                const track = mediaStream.getTracks()
+                setTracks(track[0])
+            }).catch(err => console.log(err))
+        }
+    }
