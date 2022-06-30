54e8ba906d9bf364180df5279da17e73f3e3fd5e
Rachhya Sherchan <rachhyasherchan1@gmail.com>
2022-06-30 22:49:02 +0545

    message

--- SocialMedia-master/controllers/userCtrl.js
+++ SocialMedia-master/controllers/userCtrl.js
@@ -1,50 +1,101 @@
-const mongoose = require('mongoose')
+const Users = require('../models/userModel')
+
+const userCtrl = {
+    searchUser: async (req, res) => {
+        try {
+            const users = await Users.find({username: {$regex: req.query.username}})
+            .limit(10).select("fullname username avatar")
+            
+            res.json({users})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.params.id).select('-password')
+            .populate("followers following", "-password")
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+            
+            res.json({user})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    updateUser: async (req, res) => {
+        try {
+            const { avatar, fullname, mobile, address, story, website, gender } = req.body
+            if(!fullname) return res.status(400).json({msg: "Please add your full name."})
+
+            await Users.findOneAndUpdate({_id: req.user._id}, {
+                avatar, fullname, mobile, address, story, website, gender
+            })
+
+            res.json({msg: "Update Success!"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    follow: async (req, res) => {
+        try {
+            const user = await Users.find({_id: req.params.id, followers: req.user._id})
+            if(user.length > 0) return res.status(500).json({msg: "You followed this user."})
+
+            const newUser = await Users.findOneAndUpdate({_id: req.params.id}, { 
+                $push: {followers: req.user._id}
+            }, {new: true}).populate("followers following", "-password")
+
+            await Users.findOneAndUpdate({_id: req.user._id}, {
+                $push: {following: req.params.id}
+            }, {new: true})
+
+            res.json({newUser})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    unfollow: async (req, res) => {
+        try {
+
+            const newUser = await Users.findOneAndUpdate({_id: req.params.id}, { 
+                $pull: {followers: req.user._id}
+            }, {new: true}).populate("followers following", "-password")
+
+            await Users.findOneAndUpdate({_id: req.user._id}, {
+                $pull: {following: req.params.id}
+            }, {new: true})
+
+            res.json({newUser})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    suggestionsUser: async (req, res) => {
+        try {
+            const newArr = [...req.user.following, req.user._id]
+
+            const num  = req.query.num || 10
+
+            const users = await Users.aggregate([
+                { $match: { _id: { $nin: newArr } } },
+                { $sample: { size: Number(num) } },
+                { $lookup: { from: 'users', localField: 'followers', foreignField: '_id', as: 'followers' } },
+                { $lookup: { from: 'users', localField: 'following', foreignField: '_id', as: 'following' } },
+            ]).project("-password")
+
+            return res.json({
+                users,
+                result: users.length
+            })
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+}
 
 
-const userSchema = new mongoose.Schema({
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 25
-    },
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 25,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar:{
-        type: String,
-        default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
-    },
-    role: {type: String, default: 'user'},
-    gender: {type: String, default: 'male'},
-    mobile: {type: String, default: ''},
-    address: {type: String, default: ''},
-    story: {
-        type: String, 
-        default: '',
-        maxlength: 200
-    },
-    website: {type: String, default: ''},
-    followers: [{type: mongoose.Types.ObjectId, ref: 'user'}],
-    following: [{type: mongoose.Types.ObjectId, ref: 'user'}],
-    saved: [{type: mongoose.Types.ObjectId, ref: 'user'}]
-}, {
-    timestamps: true
-})
-
-
-module.exports = mongoose.model('user', userSchema)
+module.exports = userCtrl
