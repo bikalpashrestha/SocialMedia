1075ef848e93477696fdeb4295dfe59ad92694a3
aairothapaliya <aairo2077@gmail.com>
2022-06-30 18:38:59 +0545

    reply function

---
+++ frontend/src/components/home/Comments.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import CommentDisplay from './comments/CommentDisplay'
+
+const Comments = ({post}) => {
+  
+
+    return (
+        <div className="comments">
+            e comments...
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Comments
---
+++ frontend/src/components/home/comments/CommentDisplay.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+import CommentCard from './CommentCard'
+
+const CommentDisplay = ({comment, post, replyCm}) => {
+    const [showRep, setShowRep] = useState([])
+    
+
+    return (
+        <div className="comment_display">
+           
+
+                </div>
+            </CommentCard>
+        </div>
+    )
+}
+
+export default CommentDisplay
