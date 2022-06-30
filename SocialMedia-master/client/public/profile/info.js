43f96d1e49abf69c0e436c5388c20e699ae269c9
Rachhya Sherchan <rachhyasherchan1@gmail.com>
2022-06-30 17:30:55 +0545

    delete

---
+++ SocialMedia-master/client/public/profile/info.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Info from'../../compoments/[rofile/Info'
+const Profile=()=>{
+    return(
+        <div className="profile">
+            <Info/>
+            <Posts/>
+            </div>
+        </div>
+    )
+
+
+}
+export default Profile
---
+++ SocialMedia-master/client/routes/action/profileAction.js
@@ -0,0 +1,6 @@
+import{Global TYPES}from'./globalTypes'
+export const PROFILE_TYPES={
+    LOADING:'LOADING',
+    GET_USER:'GET_USER'
+
+}
