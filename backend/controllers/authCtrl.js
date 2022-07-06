f7446bf3203e3adcfb627d5276b3ffa3c5ac9d4c
bikalpashrestha <bikalpashrestha357@gmail.com>
2022-07-06 22:40:59 +0545

    admin panel backend updated

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -88,32 +88,7 @@ const authCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
-    generateAccessToken: async (req, res) => {
-        try {
-            const rf_token = req.cookies.refreshtoken
-            if(!rf_token) return res.status(400).json({msg: "Please login now."})
-
-            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
-                if(err) return res.status(400).json({msg: "Please login now."})
-
-                const user = await Users.findById(result.id).select("-password")
-                .populate('followers following', 'avatar username fullname followers following')
-
-                if(!user) return res.status(400).json({msg: "This does not exist."})
-
-                const access_token = createAccessToken({id: result.id})
-
-                res.json({
-                    access_token,
-                    user
-                })
-            })
-            
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    }
-}
+  
 
 
 const createAccessToken = (payload) => {
