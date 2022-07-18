d610a2698060639109b41965e350c436c6ddc09c
Rachhya Sherchan <rachhyasherchan1@gmail.com>
2022-07-19 01:06:52 +0545

    like

--- client/public/src/components/header/Menu.js
+++ client/public/src/components/header/Menu.js
@@ -6,12 +6,7 @@ import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 import Avatar from '../Avatar'
 import NotifyModal from '../NotifyModal'
 
-const Menu = () => {
-    const navLinks = [
-        { label: 'Home', icon: 'home', path: '/'},
-        { label: 'Message', icon: 'near_me', path: '/message'},
-        { label: 'Discover', icon: 'explore', path: '/discover'}
-    ]
+
 
     const { auth, theme, notify } = useSelector(state => state)
     const dispatch = useDispatch()
